62cf9f28d0572ec4055d1e8296b6488cc6dd5b8c
Gvozzdd <156453933+Gvozzdd@users.noreply.github.com>
2025-04-11 14:32:24 +0500

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,1 +1,6 @@
-# Django WSGI placeholder
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+application = get_wsgi_application()
